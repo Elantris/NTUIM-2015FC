6c0e5425c55d3c6e19a2a19194c7f43352ffe3eb
Elantris <j88107333@gmail.com>
2015-03-24 23:16:20 +0800

    20150324/0.1.1/form

--- js/form.js
+++ js/form.js
@@ -1,5 +1,30 @@
-$(document).ready(function() {
-	$('.dropdown').dropdown({
-		transition: 'fade'
-	});
+$('#Thanks').hide();
+$('.dropdown').dropdown({
+	transition: 'slide down'
 });
+$('#entry-677460826').attr('value', $('#Version').html());
+
+function checkForm() {
+	$('#SignUpForm').addClass('loading');
+	var flag = true;
+	var inputId = [
+		'#entry-1152289928',
+		'#entry-357541403',
+		'#entry-837454648'
+	];
+	for (var i = 0; i < inputId.length && flag; i++) {
+		var tmp = $(inputId[i]).val();
+		if (tmp == '' || tmp == null) {
+			flag = false;
+		}
+	}
+	if (flag) {
+		$('#SignUpForm').removeClass('error');
+		$('#Thanks').fadeIn(500);
+		$('#SignUp').fadeOut(500);
+	} else {
+		$('#SignUpForm').addClass('error');
+		$('#SignUpForm').removeClass('loading');
+	}
+	return flag;
+}
